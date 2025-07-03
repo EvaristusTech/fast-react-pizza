1c3f7f079da6acd65f6f818f0127160591d6175e
EvaristusTech <arinzeevaristus640@gmail.com>
2025-07-03 17:02:08 +0100

    feat: enhance cart functionality with Redux integration and add delete item feature

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,15 @@
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button"
+import { deleteItem } from "./cartSlice";
+
+function DeleteItem({pizzaId}) {
+    const dispatch = useDispatch();
+     function handleDeleteCartItem(){
+    dispatch(deleteItem(pizzaId));
+  }
+    return (
+        <Button type="small" onClick={handleDeleteCartItem}>Delete</Button>
+    )
+}
+
+export default DeleteItem
